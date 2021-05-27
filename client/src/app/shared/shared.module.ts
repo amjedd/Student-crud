f33a2e6fe62b06c3a94ed766fbe97c91f0fc3e0d
Amjed <amjed1718@gmail.com>
2021-05-27 15:23:04 +0200

    Client : Add angular folders architecture

---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
